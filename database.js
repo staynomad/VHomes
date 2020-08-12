e074183833b575f79bde6a5c85d281da5807a182
aidenszeto <aszeto35@gmail.com>
2020-08-12 12:10:38 -0700

    dotenv

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const {MongoClient} = require('mongodb');
 require('dotenv').config();
 
 async function main(){
-    const uri = "mongodb+srv://vhomesgroup:vhomes2019@cluster0.rmikc.mongodb.net/VHomes?retryWrites=true&w=majority";
+    const uri = process.env.URI;
     const client = new MongoClient(uri);
     try {
         await client.connect();
