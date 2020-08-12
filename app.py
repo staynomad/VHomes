d16f4051932c0a22baf5d65d1d8dc992a10efc1d
aidenszeto <aszeto35@gmail.com>
2020-08-12 15:35:59 -0700

    mongo sign up functionality

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
 import pymongo
 import dns
 
+load_dotenv()
+URI = os.getenv('URI')
 app = Flask(__name__)
 
 @app.route('/')
 def signup():
-    name = request.args.get('name');
-    email = request.args.get('email');
-    password = request.args.get('password');
+    return render_template('signup.html');
+
+@app.route('/signup_success')
+def success():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    password = request.args.get('password')
 
     inputs = {
         'name': name,
@@ -17,15 +25,14 @@ def signup():
     }
 
     def add_to_db(inputs):
-        my_client = pymongo.MongoClient('mongodb+srv://vhomesgroup:vhomes2019@cluster0.rmikc.mongodb.net/VHomes?retryWrites=true&w=majority')
+        my_client = pymongo.MongoClient(URI)
         my_db = my_client['VHomes']
         my_col = my_db['users']
         my_col.insert_one(inputs)
 
     add_to_db(inputs);
-
-    return render_template('signup.html');
-
-@app.route('/signup_success')
-def success():
     return render_template('signup_success.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
