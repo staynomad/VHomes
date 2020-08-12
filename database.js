8a4bdd2116ad77be7b22ac99f3b1797b64d62d45
aidenszeto <aszeto35@gmail.com>
2020-08-12 11:58:43 -0700

    database.js

--- database.js
+++ database.js
@@ -1,3 +1,29 @@
-function addUser() {
-  
+const {MongoClient} = require('mongodb');
+
+async function main(){
+    const uri = "mongodb+srv://vhomesgroup:vhomes2019@cluster0.rmikc.mongodb.net/VHomes?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        await  listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
+
+async function addUser(client){
+  const uri = "mongodb+srv://vhomesgroup:vhomes2019@cluster0.rmikc.mongodb.net/VHomes?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+      await client.connect();
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+};
+
+main().catch(console.error);
