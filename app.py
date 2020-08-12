790cd783d7c710ecc151a9664404313714595c6d
aidenszeto <aszeto35@gmail.com>
2020-08-12 15:49:46 -0700

    full functionality

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def signup():
     return render_template('signup.html');
 
 @app.route('/signup_success')
-def success():
+def signup_success():
     name = request.args.get('name')
     email = request.args.get('email')
     password = request.args.get('password')
@@ -36,3 +36,26 @@ def success():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/login_success')
+def login_success():
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    inputs = {
+        'email': email,
+        'password': password
+    }
+
+    def get_from_db(email, password):
+        my_client = pymongo.MongoClient(URI)
+        my_db = my_client['VHomes']
+        my_col = my_db['users']
+        user = my_col.find_one({'email': str(email)}, {'password': str(password)})
+        return(user)
+
+    if get_from_db(email, password) == None:
+        response = 'incorrect username or password'
+    else:
+        response = 'logged in as ' + email
+    return render_template('login_success.html', response=response)
