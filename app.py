8c8c0640f676dfcd771fde75e343bd58839b220f
aidenszeto <aszeto35@gmail.com>
2020-09-03 11:05:32 -0700

    signup unsuccessful html

--- app.py
+++ app.py
@@ -5,35 +5,43 @@ import pymongo
 import dns
 import aes
 
+
 load_dotenv()
 URI = os.getenv('URI')
 KEY = os.getenv('KEY')
 cipher = aes.AESCipher(KEY)
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('home.html');
+
 
 @app.route('/home')
 def home():
     return render_template('home.html');
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html');
+
 
 @app.route('/locations')
 def locations():
     return render_template('locations.html');
 
+
 @app.route('/services')
 def services():
     return render_template('services.html')
 
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html');
+
 
 @app.route('/signup_success')
 def signup_success():
@@ -49,18 +57,32 @@ def signup_success():
         'password': enc_pass
     }
 
+    def check_db(email):
+        my_client = pymongo.MongoClient(URI)
+        my_db = my_client['VHomes']
+        my_col = my_db['users']
+        user = my_col.find_one({'email': str(email)})
+        return user
+
     def add_to_db(inputs):
         my_client = pymongo.MongoClient(URI)
         my_db = my_client['VHomes']
         my_col = my_db['users']
         my_col.insert_one(inputs)
 
-    add_to_db(inputs);
-    return render_template('signup_success.html')
+    if check_db(email) == None:
+        add_to_db(inputs)
+        response = 'sign up successful!'
+        return render_template('signup_unsuccessful.html', response=response)
+    else:
+        response = 'email already registered'
+        return render_template('signup_success.html', response=response)
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 
 @app.route('/login_success')
 def login_success():
@@ -89,6 +111,7 @@ def login_success():
     else:
         response = 'logged in as ' + email
         return render_template('login_success.html', response=response)
+
 
 if __name__ == '__main__':
     app.run()
